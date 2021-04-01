68c3d1a42cd73d404206d413cf4d6433d38c8903
mwtas <mwtasemalkojak@gmail.com>
2021-04-01 03:03:11 -0400

    I  request in Postman and make sure you're getting the right JSON back, and create a new user through the API. Below is a start for a create action , POST request to create a new user with Postman , To inform the user of what went wrong, it is typical to send back error messages ,

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,35 @@ class UsersController < ApplicationController
     end 
 
     def create
-        render json: params
+        # debugger
+        user = User.new(user_params)
+        # replace the `user_attributes_here` with the actual attribute keys
+       if  user.save
+        render json: user
+       else
+        render json: user.errors.full_messages, status: :unprocessable_entity
+       end 
     end
 
-      def show 
-        # render json: User.find(params[:id])
-        render json: params
-      end 
+    def show
+        render json: User.find(params[:id])
+      end
+    
+      def update
+        user = User.find(params[:id])
+        if user.update_attributes(user_params)
+          render json: user
+        else
+          render json: user.errors, status: :unprocessable_entity
+        end
+      end
+
+      
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name, :email)
+      end
+   
 end
