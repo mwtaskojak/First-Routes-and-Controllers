c5a7a785f545e002fccb8b7fabf07154c4a808e6
mwtas <mwtasemalkojak@gmail.com>
2021-04-01 01:49:27 -0400

    Make the request in Postman and make sure you're getting the right JSON back UsersController#index, fetch all the users from the database and render them as JSON , web APIs, are all about connecting data in your database with the outside world. You've just done that.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+    #     render plain: "I'm in the index action!"
+    render json: User.all    
+    end 
+
+    def create
+        render json: params
     end
 
-    # def create
-    #     render json: params
-    # end
-
       def show 
-        render json: User.find(params[:id])
+        # render json: User.find(params[:id])
+        render json: params
       end 
 end
