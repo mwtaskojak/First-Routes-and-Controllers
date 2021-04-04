f03ac3fb8b95fa7649d9c4e66190ffe5110403e2
mwtas <mwtasemalkojak@gmail.com>
2021-04-03 23:46:05 -0400

    Creat model like  There is a :scope option that you can use to specify one or more attributes that are used to limit the uniqueness

---
+++ db/migrate/20210404034141_creat_likes.rb
@@ -0,0 +1,13 @@
+class CreatLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+      t.integer :user_id
+      t.references :likeable, polymorphic: true, index: true
+
+      t.timestamps
+    end
+
+    add_index :likes, [:user_id, :likeable_type, :likeable_id], unique: true
+
+  end
+end
