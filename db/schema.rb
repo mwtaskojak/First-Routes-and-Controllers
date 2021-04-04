c1613fbaefcafe745aabfe4c19c366555f31d98e
mwtas <mwtasemalkojak@gmail.com>
2021-04-04 13:40:27 -0400

    Create table artwork_colletion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_034141) do
+ActiveRecord::Schema.define(version: 2021_04_04_040030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2021_04_04_034141) do
     t.index ["artist_id"], name: "index_artworks_on_artist_id"
     t.index ["image_url"], name: "index_artworks_on_image_url", unique: true
     t.index ["title", "artist_id"], name: "index_artworks_on_title_and_artist_id", unique: true
+  end
+
+  create_table "collections", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
